4cf8f081d4a28ecc9b232648c627bb7b598cc33d
Deivid Veloso <veloso.deivid@gmail.com>
2017-08-15 09:34:41 -0300

    Initial project Eventos.IO

---
+++ AspNetCoreEdu/DemoIdentity/DemoIdentity/Services/ISmsSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoIdentity.Services
+{
+    public interface ISmsSender
+    {
+        Task SendSmsAsync(string number, string message);
+    }
+}
