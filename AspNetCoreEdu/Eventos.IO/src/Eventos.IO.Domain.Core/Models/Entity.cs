df451fe6bea5c350f5db48de4800c65671ba7eba
Deivid Veloso <veloso.deivid@gmail.com>
2017-08-21 09:45:29 -0300

    Create Props Class Eventos

---
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain.Core/Models/Entity.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Core.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; protected set; }
+
+        public override bool Equals(object obj)
+        {
+            var compareTo = obj as Entity;
+
+            if (ReferenceEquals(this, compareTo)) return true;
+            if (ReferenceEquals(null, compareTo)) return false;
+
+            return Id.Equals(compareTo.Id);
+        }
+
+        public static bool operator ==(Entity a, Entity b)
+        {
+            if (ReferenceEquals(a, null) && ReferenceEquals(b, null))
+                return true;
+
+            if (ReferenceEquals(a, null) || ReferenceEquals(b, null))
+                return false;
+
+            return a.Equals(b);
+        }
+
+        public static bool operator !=(Entity a, Entity b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            //Conhecido como Magic number, é usado para gerar um HashCode único para classe.
+            return (GetType().GetHashCode() * 907) + Id.GetHashCode();
+        }
+
+        //Usado para quando eu der um ToString identificar qual classe e Id estou chamando.
+        public override string ToString()
+        {
+            return GetType().Name + "[Id = ]" + Id + "]";
+        }
+    }
+}
--- AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/Models/Eventos.cs
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/Models/Eventos.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using Eventos.IO.Domain.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Eventos.IO.Domain.Models
 {
-    public class Eventos
+    public class Eventos : Entity
     {
+        public string Nome { get; private set; }
+        public string DescricaoCurta { get; private set; }
+        public string DescricaoLonga { get; private set; }
+        public DateTime DataIncio { get; private set; }
+        public DateTime DataFim { get; private set; }  
+        public bool Gratuito { get; private  set; }
+        public decimal Valor { get; private set; }
+        public bool Online { get; private set; }
+        public string NomeEmpresa { get; private set; }
+        public Categoria Categoria { get; private set; }
+        public ICollection<Tags> Tags { get; private set; }
+        public Endereco Endereco { get; private set; }
+        public Organizador Organizador { get; private set; }
+    }
 
+    public class Teste
+    {
+        public Teste()
+        {
+            var eventos = new Eventos();
+            eventos.Nome = "Show"; 
+        }
     }
 }
