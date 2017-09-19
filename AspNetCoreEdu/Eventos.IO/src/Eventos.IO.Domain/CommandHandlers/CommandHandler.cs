88ce5717a95af7a0d29ecc931ed4cfe25bd69bce
Deivid Veloso <veloso.deivid@gmail.com>
2017-09-19 20:40:54 -0300

    command handlers

---
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/CommandHandlers/CommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.CommandHandlers
+{
+    public abstract class CommandHandler
+    {
+        protected void Notificacoes(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/Eventos/CommandHandlers/EventoCommandHandler.cs
@@ -0,0 +1,57 @@
+﻿using Eventos.IO.Domain.CommandHandlers;
+using Eventos.IO.Domain.Core.Events;
+using Eventos.IO.Domain.Eventos.Commands;
+using Eventos.IO.Domain.Eventos.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Domain.Eventos.CommandHandlers
+{
+    public class EventoCommandHandler : CommandHandler,
+        IHandler<RegistrarEventoCommand>,
+        IHandler<AtualizarEventoCommand>,
+        IHandler<ExcluirEventoCommand>
+    {
+        private readonly IEventoRepository _eventoRepository;
+
+        public EventoCommandHandler(IEventoRepository eventoRepository)
+        {
+            _eventoRepository = eventoRepository;
+        }
+
+        public void Handle(RegistrarEventoCommand message)
+        {
+            var evento = new Evento(
+                message.Nome,
+                message.DataIncio,
+                message.DataFim,
+                message.Gratuito,
+                message.Valor,
+                message.Online,
+                message.NomeEmpresa
+                );
+
+            if (!evento.EhValido())
+            {
+                Notificacoes(evento.ValidationResult);
+                return;
+            }
+
+            //Posso colocar mais validações
+
+            //Aqui salvo no meu banco de dados as informações de entrada.
+            _eventoRepository.Add(evento);
+        }
+        
+        public void Handle(AtualizarEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ExcluirEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
