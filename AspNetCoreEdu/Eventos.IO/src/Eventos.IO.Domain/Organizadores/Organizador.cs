16938752579fb42f6f32e238ff9d39a891187fb3
Deivid Veloso <veloso.deivid@gmail.com>
2017-11-08 09:44:50 -0200

    Class 13 time 20:00

--- AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/Organizadores/Organizador.cs
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Domain/Organizadores/Organizador.cs
@@ -1,19 +1,35 @@
 ﻿using System;
 using Eventos.IO.Domain.Core.Models;
+using System.Collections.Generic;
+using Eventos.IO.Domain.Eventos;
 
 namespace Eventos.IO.Domain.Organizadores
 {
     public class Organizador : Entity<Organizador>
     {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+        public string Email { get; private set; }
 
-        public Organizador(Guid id)
+        public virtual ICollection<Evento> Eventos { get; set; }
+
+
+        public Organizador(Guid id, string nome, string cpf, string email)
         {
             Id = id;
+            Nome = nome;
+            CPF = cpf;
+            Email = email;
+        }
+
+        protected Organizador()
+        {
+
         }
 
         public override bool EhValido()
         {
-            throw new NotImplementedException();
+            return true;
         }
     }
 }
---
+++ AspNetCoreEdu/Eventos.IO/src/Eventos.IO.Infra.Data/Context/EventosContext.cs
@@ -0,0 +1,71 @@
+﻿using Eventos.IO.Domain.Eventos;
+using Eventos.IO.Domain.Organizadores;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eventos.IO.Infra.Data.Context
+{
+    public class EventosContext : DbContext
+    {
+        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<Organizador> Organizadores { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            #region FlentAPI
+
+            #region Evento
+            modelBuilder.Entity<Evento>()
+                .ToTable("Eventos");
+
+            modelBuilder.Entity<Evento>()
+                .Property(e => e.Nome)
+                .HasColumnType("varchar(150)")
+                .IsRequired();
+
+            modelBuilder.Entity<Evento>()
+             .Property(e => e.DescricaoCurta)
+             .HasColumnType("varchar(150)");
+
+            modelBuilder.Entity<Evento>()
+             .Property(e => e.DescricaoLonga)
+             .HasColumnType("varchar(150)");
+
+            modelBuilder.Entity<Evento>()
+             .Property(e => e.NomeEmpresa)
+             .HasColumnType("varchar(150)")
+             .IsRequired();
+
+            modelBuilder.Entity<Evento>()
+                .Ignore(e => e.ValidationResult);
+
+            modelBuilder.Entity<Evento>()
+                .Ignore(e => e.Tags);
+
+            //Pertence ao fluentvalidation
+            modelBuilder.Entity<Evento>()
+                .Ignore(e => e.CascadeMode);
+
+            //Mapear os relacionamentos
+            modelBuilder.Entity<Evento>()
+               .HasOne(e => e.Organizador) //Um evento só pode ter um Organizador
+               .WithMany(o => o.Eventos) //Um organizador pode ter varios eventos.
+               .HasForeignKey(e => e.OrganizadorId); //Evento vai ter uma FK com Organizador Evento.OrganizadorId = Organizador.organizadorId
+
+            modelBuilder.Entity<Evento>()
+                .HasOne(e => e.Categoria)
+                .WithMany(c => c.Eventos)
+                .HasForeignKey(e => e.CategoriaId)
+                .IsRequired(false);
+
+            #endregion
+
+            #endregion
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+
+}
