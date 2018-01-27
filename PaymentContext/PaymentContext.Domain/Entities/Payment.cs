3829ba83fec6911f9c2df59505fb147326921af7
Deivid Veloso <veloso.deivid@gmail.com>
2018-01-27 15:09:19 -0200

    add new classes and fix List problems

--- PaymentContext/PaymentContext.Domain/Entities/Payment.cs
+++ PaymentContext/PaymentContext.Domain/Entities/Payment.cs
@@ -4,31 +4,28 @@ namespace PaymentContext.Domain.Entities
 {
     public abstract class Payment
     {
-        public int Number { get; set; }
-        public DateTime PaidDate { get; set; }
-        public DateTime ExpireDate { get; set; }
-        public decimal Total { get; set; }
-        public decimal TotalPaid { get; set; }
-        public string Address { get; set; }
-        public string Email { get; set; }
-    }
+        public Payment(DateTime paidDate, DateTime expireDate, decimal total,
+        decimal totalPaid, string payer, string document, string address, string email)
+        {
+            Number = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 10).ToUpper();
+            PaidDate = paidDate;
+            ExpireDate = expireDate;
+            Total = total;
+            TotalPaid = totalPaid;
+            Payer = payer;
+            Document = document;
+            Address = address;
+            Email = email;
+        }
 
-    public class BoletoPayment : Payment
-    {
-        public string Boleto { get; set; }
-        public string BoletoNumber { get; set; }
-    }
-
-    public class CreditCardPayment : Payment
-    {
-        //Não armazenar dados do cartão e sim utilizar um gateway de pagamento.
-        public string CardHolderName { get; set; }
-        public string CardNumber { get; set; }
-        public string LastTransactionNumber { get; set; }
-    }
-
-    public class PayPalPayment : Payment
-    {
-        public string TransactionCode { get; set; }
+        public string Number { get; private set; }
+        public DateTime PaidDate { get; private set; }
+        public DateTime ExpireDate { get; private set; }
+        public decimal Total { get; private set; }
+        public decimal TotalPaid { get; private set; }
+        public string Payer { get; private set; }
+        public string Document { get; private set; }
+        public string Address { get; private set; }
+        public string Email { get; private set; }
     }
 }
