a20cd15f3b4b280c9779dd5a3290a257f55c40fe
DeividVeloso <veloso.deivid@gmail.com>
2018-02-13 10:46:04 -0200

    add repository parttern

---
+++ PaymentContext/PaymentContext.Domain/Repositories/IStudentRepository.cs
@@ -0,0 +1,9 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Domain.Repositories {
+    public interface IStudentRepository {
+        bool DocumentExists(string document);
+        bool EmailExists(string email);
+        void CreateSebscription(Student student);
+    }
+}
